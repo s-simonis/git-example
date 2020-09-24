da7b029b79147d7a11b1b266f05b4dc5f5f30d3a
s-simonis <s.simonis@novi-education.nl>
2020-09-24 18:47:51 +0200

    Added class 'Developer'

---
+++ src/nl/novi/javaprogrammeren/Developer.java
@@ -0,0 +1,17 @@
+package nl.novi.javaprogrammeren;
+
+public class Developer {
+    private String name;
+
+    public Developer(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
